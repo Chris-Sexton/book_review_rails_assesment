c84d7d58d35f46b3c35327814a142dc2ecbe4e62
Chris-Sexton <champlain2013@gmail.com>
2016-10-14 11:19:20 -0400

    Added edit and delete button to book show page.

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,7 +1,11 @@
 class BooksController < ApplicationController
+  before_action :find_book, only: [:show, :edit, :update, :destroy]
   
   def index
     @books = Book.all.order("created_at DESC")
+  end
+
+  def show
   end
 
   def new
@@ -18,9 +22,29 @@ class BooksController < ApplicationController
     end
   end
 
+  def edit
+
+  end
+
+  def update
+    if @book.update(book_params)
+      redirect_to book_path(@book)
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+
+  end
+
   private
 
     def book_params
       params.require(:book).permit(:title, :description, :author)
+    end
+
+    def find_book
+      @book = Book.find(params[:id])
     end
 end
